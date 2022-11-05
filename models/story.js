4225f929b70ad981b698e958d6d60ccdd3e902a7
Aggio98 <nigelrizzo@gmail.com>
2022-11-05 10:55:07 +0100

    Server contains sequelize models and migrations for Space & Story

--- models/story.js
+++ models/story.js
@@ -1,27 +1,26 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
+  class story extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      user.hasOne(models.space, { foreignKey: "userId" });
+      story.belongsTo(models.space, { foreignKey: "spaceId" });
     }
   }
-  user.init(
+  story.init(
     {
-      name: { type: DataTypes.STRING, unique: true, allowNull: false },
-      email: { type: DataTypes.STRING, allowNull: false },
-      phone: { type: DataTypes.INTEGER },
-      password: { type: DataTypes.STRING, allowNull: false },
+      name: DataTypes.STRING,
+      content: DataTypes.TEXT,
+      imageUrl: DataTypes.STRING,
     },
     {
       sequelize,
-      modelName: "user",
+      modelName: "story",
     }
   );
-  return user;
+  return story;
 };
