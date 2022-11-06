df9e2ef937b31dacfab19c51b03aedae1e70bbc9
Aggio98 <nigelrizzo@gmail.com>
2022-11-06 14:35:39 +0100

    feature 2 added the endpoint to get the stories of spaces

--- routers/spaces.js
+++ routers/spaces.js
@@ -1,11 +1,26 @@
 const { Router } = require("express");
 const Space = require("../models").space;
+const Story = require("../models").story;
 const router = new Router();
 
-//F1 create a endpoint that responds with a list of spaces
+//F1 create an endpoint that responds with a list of spaces
 router.get("/spaces", async (req, res, next) => {
   const response = await Space.findAll();
   res.send(response);
 });
 
+//F2 create an endpoint that responds with one space and its stories
+router.get("/spaces/:id", async (req, res, next) => {
+  try {
+    const spaceId = parseInt(req.params.id);
+    const getSpace = await Space.findByPk(spaceId, { include: [Story] });
+    if (getSpace) {
+      res.send(getSpace);
+    } else {
+      req.status(404).send("Space not found");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 module.exports = router;
